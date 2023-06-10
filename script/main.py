890e2f4caca371f7bc2bb04663a31640a6120c1c
JJUCAT <17688010148@163.com>
2023-06-10 22:02:05 +0800

    fix(pyspider): pyspider

---
+++ script/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+
+import pyspider
+
+
+import monitor
+
+
+if __name__ == '__main__':
+    print('webcrawler <scarab> run')
+
+    
---
+++ script/stock.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+
+class Stock:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+    
